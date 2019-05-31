9976f2ce3e21cf9df81768e860c39bddba0a7b67
Isardbp <isardbp@gmail.com>
2019-06-01 01:32:10 +0200

    try1 button ionic vue

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ion-button color="primary" href="/about">Primary</ion-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
